b9f039f0137487b38106b033e87eb44890ca2b75
Arnav Gupta <arnav@codingblocks.com>
2018-05-05 16:11:13 +0530

    add stragegy optoins include property
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,24 @@
+import {OneauthStrategyOptions} from 'passport-oneauth'
+
 declare module 'passport-oneauth' {
-  import {Strategy as Oauth2Strategy} from 'passport-oauth2'
-  class OneauthStrategy extends Oauth2Strategy {
+  import {
+    Strategy as OAuth2Strategy,
+    StrategyOptions,
+    StrategyOptionsWithRequest,
+    VerifyFunction,
+    VerifyFunctionWithRequest
+  } from 'passport-oauth2'
+
+  interface OneauthStrategyOptions {
+    include?: string[]
+  }
+
+  class OneauthStrategy extends OAuth2Strategy {
+    constructor(options: StrategyOptions & OneauthStrategyOptions, verify: VerifyFunction);
+    constructor(options: StrategyOptionsWithRequest & OneauthStrategyOptions, verify: VerifyFunctionWithRequest);
 
   }
+
   export type Strategy = OneauthStrategy;
-  export const Strategy: typeof OneauthStrategy;
+  export const Strategy: typeof OneauthStrategy
 }
